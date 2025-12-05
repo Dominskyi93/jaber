d4a29aec6eeed1b0e73595459f4a475aef829508
Denys Dominskyi <dominskyj@gmail.com>
2025-12-05 10:00:35 +0200

    added common-android module

---
+++ core/common-android/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    alias(libs.plugins.custom.android.library)
+}
+
+android {
+    namespace = "com.messenger.core.common.android"
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
