bd38a0fed388778aec9b9d211a285a7a83b05e2e
Denys Dominskyi <dominskyj@gmail.com>
2025-12-16 10:36:56 +0200

    added compose to theme

--- core/theme/build.gradle.kts
+++ core/theme/build.gradle.kts
@@ -1,16 +1,30 @@
 plugins {
     alias(libs.plugins.custom.android.library)
+    alias(libs.plugins.compose)
 }
 
 android {
     namespace = "com.messenger.core.theme"
+
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
+    implementation(projects.core.essentials)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    //compose
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+
 }
