048b23aec6e4afc65792c016ba87259d74777a52
Denys Dominskyi <dominskyj@gmail.com>
2025-12-17 16:57:01 +0200

    added navigation module

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,6 +6,7 @@ dependencies {
     implementation(libs.plugin.android.application)
     implementation(libs.plugin.android.library)
     implementation(libs.plugin.kotlin.jvm)
+    implementation(libs.plugin.kotlin.serialization)
     implementation(libs.plugin.kotlin.android)
     implementation(libs.plugin.kotlin.compose)
     implementation(libs.plugin.hilt.android)
---
+++ navigation/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    alias(libs.plugins.custom.android.library)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.compose)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+android {
+    namespace = "com.messenger.jaber.navigation"
+}
+
+dependencies {
+    //hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    //compose
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation(libs.navigation.compose)
+    implementation(libs.hilt.navigation)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
