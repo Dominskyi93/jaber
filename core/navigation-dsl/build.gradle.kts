8c2174db10ee6a57e6506f398851fb73eb569f43
Denys Dominskyi <dominskyj@gmail.com>
2025-12-25 09:39:18 +0200

    added navigation-dsl module

---
+++ core/navigation-dsl/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    alias(libs.plugins.custom.android.library)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.compose)
+}
+
+android {
+    namespace = "com.messenger.jaber.core.navigation.dsl"
+}
+
+dependencies {
+    api(projects.features.init.domain)
+    implementation(projects.core.essentials)
+    implementation(projects.core.theme)
+    //hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    //compose
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation(libs.navigation.compose)
+    implementation(libs.hilt.navigation)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
