6a738194dfc5cdb89ea728f70d2723798115487c
Denys Dominskyi <dominskyj@gmail.com>
2025-12-30 09:37:12 +0200

    added core.presentation module

--- core/presentation/build.gradle.kts
+++ core/presentation/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.custom.android.library)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
     alias(libs.plugins.compose)
 }
 
@@ -13,13 +15,11 @@ android {
 
 dependencies {
     implementation(projects.core.essentials)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    //coil
-    implementation(libs.coil.compose)
-    implementation(libs.coil.okhttp)
+    implementation(projects.core.theme)
+    implementation(projects.core.navigationDsl)
+    //hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
     //compose
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
@@ -29,9 +29,14 @@ dependencies {
     implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-    //test
+    //navigation
+    implementation(libs.navigation.compose)
+    implementation(libs.hilt.navigation)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
 }
