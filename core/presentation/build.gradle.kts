f134caa586c80f9b52a2623c0181561fbda49601
Denys Dominskyi <dominskyj@gmail.com>
2025-12-30 09:10:00 +0200

    added core.presentation module

---
+++ core/presentation/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    alias(libs.plugins.custom.android.library)
+    alias(libs.plugins.compose)
+}
+
+android {
+    namespace = "com.messenger.jaber.core.presentation"
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(projects.core.essentials)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    //coil
+    implementation(libs.coil.compose)
+    implementation(libs.coil.okhttp)
+    //compose
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    //test
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+}
