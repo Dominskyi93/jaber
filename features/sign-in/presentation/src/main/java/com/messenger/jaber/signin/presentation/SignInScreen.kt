8b2b2070034f2215e6709c94edf6ccdd00660b39
Denys Dominskyi <dominskyj@gmail.com>
2026-01-03 12:56:03 +0200

    added image to sign in

--- features/sign-in/presentation/src/main/java/com/messenger/jaber/signin/presentation/SignInScreen.kt
+++ features/sign-in/presentation/src/main/java/com/messenger/jaber/signin/presentation/SignInScreen.kt
@@ -1,9 +1,12 @@
 package com.messenger.jaber.signin.presentation
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
@@ -18,6 +21,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
@@ -58,7 +62,7 @@ fun SignInContent(
     onClearErrorMessages: () -> Unit
 ) {
     var email by rememberSaveable { mutableStateOf("") }
-    var password by rememberSaveable() { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
 
     Column(
         verticalArrangement = Arrangement.spacedBy(
@@ -70,6 +74,13 @@ fun SignInContent(
             .padding(Dimens.MediumPadding)
             .fillMaxSize()
     ) {
+        Image(
+            painter = painterResource(R.drawable.ic_network),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .aspectRatio(1f)
+        )
         OutlinedTextField(
             value = email,
             onValueChange = {
