ce081f89f3a99821414e72e024917be1f509dc4d
Denys Dominskyi <dominskyj@gmail.com>
2026-01-09 17:36:49 +0200

    added SignUpUseCaseTest

---
+++ features/signup/domain/src/test/java/com/messenger/jaber/features/signup/domain/usecases/SignUpUseCaseTest.kt
@@ -0,0 +1,65 @@
+package com.messenger.jaber.features.signup.domain.usecases
+
+import com.messenger.jaber.features.signup.domain.entities.NewAccount
+import com.messenger.jaber.features.signup.domain.entities.ValidationResult
+import com.messenger.jaber.features.signup.domain.exceptions.base.AbstractValidationException
+import com.messenger.jaber.features.signup.domain.repositories.CreateAccountRepository
+import com.messenger.jaber.features.signup.domain.validators.NewAccountValidator
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+class SignUpUseCaseTest {
+    @MockK
+    private lateinit var validator: NewAccountValidator
+
+    @RelaxedMockK
+    private lateinit var repository: CreateAccountRepository
+    private lateinit var useCase: SignUpUseCaseImpl
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        useCase = SignUpUseCaseImpl(
+            newAccountValidator = validator,
+            createAccountRepository = repository
+        )
+    }
+
+    @Test
+    fun `creates account when validation is successful`() = runTest {
+        val account = mockk<NewAccount>()
+
+        coEvery { validator.validate(account) } returns ValidationResult.Success
+
+        useCase.invoke(account)
+
+        coVerify(exactly = 1) {
+            repository.createAccount(account)
+        }
+    }
+
+    @Test(expected = AbstractValidationException::class)
+    fun `throws first validation exception and does not create account`() = runTest {
+        val account = mockk<NewAccount>()
+        val exception = mockk<AbstractValidationException>()
+
+        val failure = ValidationResult.Failure(
+            exceptions = listOf(exception)
+        )
+
+        coEvery { validator.validate(account) } returns failure
+
+        useCase.invoke(account)
+
+        coVerify(exactly = 0) {
+            repository.createAccount(any())
+        }
+    }
+}
