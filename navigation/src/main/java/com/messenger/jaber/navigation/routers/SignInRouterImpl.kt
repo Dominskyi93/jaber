2dff0212bc96443638d4baee8f88c515da2b25a3
Denys Dominskyi <dominskyj@gmail.com>
2026-01-24 10:50:49 +0200

    fixed navigate after authrizing

--- navigation/src/main/java/com/messenger/jaber/navigation/routers/SignInRouterImpl.kt
+++ navigation/src/main/java/com/messenger/jaber/navigation/routers/SignInRouterImpl.kt
@@ -1,16 +1,13 @@
 package com.messenger.jaber.navigation.routers
 
-import android.content.Context
 import com.messenger.jaber.navigation.ChatsRoute
 import com.messenger.jaber.navigation.SignUpRoute
 import com.messenger.jaber.navigation.base.AppRouter
 import com.messenger.jaber.signin.presentation.SignInRouter
-import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 class SignInRouterImpl @Inject constructor(
     private val appRouter: AppRouter,
-    @ApplicationContext private val context: Context
 ) : SignInRouter {
 
     override fun navigateUp() {
