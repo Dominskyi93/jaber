882a86059bd1696f0fbbc17e31f6dc58b49cd8b4
Denys Dominskyi <dominskyj@gmail.com>
2026-02-24 15:52:17 +0200

    live update added to chats

--- data/src/main/java/com/messenger/jaber/data/rooms/RoomsDataRepositoryImpl.kt
+++ data/src/main/java/com/messenger/jaber/data/rooms/RoomsDataRepositoryImpl.kt
@@ -1,16 +1,13 @@
 package com.messenger.jaber.data.rooms
 
 import com.elveum.container.ListContainerFlow
-import com.elveum.container.containerOf
+import com.elveum.container.errorContainer
 import com.elveum.container.successContainer
-import com.elveum.container.unwrap
 import com.google.firebase.firestore.FirebaseFirestore
-import com.messenger.core.essentials.entities.Id
 import com.messenger.jaber.data.RoomsDataRepository
 import com.messenger.jaber.data.rooms.entities.RoomDataEntity
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -18,14 +15,23 @@ import javax.inject.Singleton
 class RoomsDataRepositoryImpl @Inject constructor(
     private val firestore: FirebaseFirestore
 ) : RoomsDataRepository {
-    override fun getRooms(): ListContainerFlow<RoomDataEntity> = flow {
-        val result = successContainer(
-            firestore.collection("chats")
-                .get()
-                .await()
-                .documents
-                .mapNotNull { it.toObject(RoomDataEntity.Default::class.java) }
-        )
-        emit(result)
+    override fun getRooms(): ListContainerFlow<RoomDataEntity> = callbackFlow {
+        val listenerRegistration = firestore
+            .collection("chats")
+            .addSnapshotListener { snapshot, error ->
+
+                if (error != null) {
+                    trySend(errorContainer(error))
+                    return@addSnapshotListener
+                }
+
+                val data = snapshot?.documents
+                    ?.mapNotNull { it.toObject(RoomDataEntity.Default::class.java) }
+                    ?: emptyList()
+
+                trySend(successContainer(data))
+            }
+
+        awaitClose { listenerRegistration.remove() }
     }
 }
